1b82589da0546bd5c7e9b15af9fdbe7d6afb808c
Shura <petrovai_net@mail.ru>
2022-08-25 13:29:03 +0300

    added files...

---
+++ types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using ip_pool_vector	= std::vector<std::vector<int>>;
+using string_vector		= std::vector<std::string>;
+using int_vector		= std::vector<int>;
+
